546fdf53ba5bd92bb8543ed0f307938367bd4fef
Giuseppe Pascale <pascale.giuseppe03@gmail.com>
2023-12-11 04:35:41 +0100

    chore: moving to next

--- apps/buyer/src/lib/constants.js
+++
@@ -1,8 +0,0 @@
-// API Endpoints
-
-export const API_URL = "https://bitshop.fadibarbara.it";
-export const BLASTR_EP = `${API_URL}/event`;
-export const EVENT_KINDS = {
-  PUBLISH_STALL: 300170, // 30017 is the real event kind
-  PUBLISH_PRODUCT: 300180, // 30018 is the real event kind
-};
